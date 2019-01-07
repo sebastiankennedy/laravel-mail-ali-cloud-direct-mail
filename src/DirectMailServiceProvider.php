5668ed085768cc45c14f53b41faa208948732f7f
Sebastian <sebastiankennedy@foxmail.com>
2019-01-07 23:22:03 +0800

    Revert "Update service provider"
    This reverts commit f1f8a6812e069ffa182d2ed0650098cc61d79b2e.

--- src/DirectMailServiceProvider.php
+++ src/DirectMailServiceProvider.php
@@ -18,14 +18,6 @@ class DirectMailServiceProvider extends ServiceProvider
         $this->app['swift.transport']->extend('ali_cloud_direct_mail', function () {
             $config = $this->app['config']->get('services.ali_cloud_direct_mail', []);
 
-            if (!$config) {
-                // TODO 缺少配置参数
-            }
-
-            if (array_keys(['access_key_id', 'access_key_secret', 'from_alias', 'account_name'], $config)) {
-                // TODO 缺少必要配置参数
-            }
-
             return new DirectMailTransport(new Client(), $config['access_key_secret'], $config);
         });
     }
